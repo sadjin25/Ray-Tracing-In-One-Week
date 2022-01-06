fb8488331ca496065662ae0aa169dad9ba3b4aad
sadjin25 <qkrdnwls0101@naver.com>
2022-01-07 00:49:33 +0900

    red sphere

--- 1.cpp
+++ 1.cpp
@@ -4,7 +4,19 @@
 
 #include <iostream>
 
+bool hitSphere(const point3& center, double radius, const ray& r)  {
+    vec3 oc = r.origin() - center;
+    auto a = dot(r.direction(), r.direction());
+    auto b = 2.0 * dot(oc, r.direction());
+    auto c = dot(oc, oc) - radius*radius;
+    auto discriminant = b*b - 4*a*c;
+    return (discriminant > 0);
+}
+
 color rayColor(const ray& r) {
+    if(hitSphere(point3(0,0,-1), 0.5, r)) {
+        return color(1, 0, 0);
+    }
     vec3 unitDirection = unitVector(r.direction());
     auto t = 0.5*(unitDirection.y() + 1.0);
     return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
